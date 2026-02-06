00e3f6598d572e4ff2948860ac3e3d9fb60fa36d
Abhinand <abhinandtk.primalcodes@gmail.com>
2026-02-06 23:40:25 +0530

    product model created

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import User,Product,ProductVariant
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model=User
+    fieldsets = UserAdmin.fieldsets + (
+        ( "Additional Info", {
+            "fields" : ("dob" , "address" , "phone" ),
+        } ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {
+            "fields": ("dob", "address", "phone"),
+        }),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "phone",
+        "is_staff",
+    )
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("Basic Info", {
+            "fields": ("name", "description"),
+        }),
+        ("Pricing & Inventory", {
+            "fields": ("base_price",),
+        }),
+        ("Status", {
+            "fields": ("is_active",),
+        }),
+    )
+
+    list_display = ("name", "base_price", "is_active",)
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+    inlines=[ProductVariantInline]
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -6,3 +6,43 @@ class User(AbstractUser):
     dob = models.DateField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     phone = models.CharField(max_length=15, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class ProductVariant(models.Model):
+    SIZE_CHOICES = [
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
+        ("XL", "Extra Large"),
+    ]
+
+    COLOR_CHOICES = [
+        ("red", "Red"),
+        ("blue", "Blue"),
+        ("black", "Black"),
+    ]
+
+    product = models.ForeignKey(
+        'Product',
+        related_name="variants",
+        on_delete=models.CASCADE
+    )
+    size = models.CharField(max_length=5, choices=SIZE_CHOICES)
+    color = models.CharField(max_length=20, choices=COLOR_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ("product", "size", "color")
+
+    def __str__(self):
+        return f"{self.product.name} - {self.size} - {self.color}"

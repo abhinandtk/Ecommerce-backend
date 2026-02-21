c03ec6b6ec6d86b2edacdcfff4b4f96cf1dfa231
abhinandtk.primalcodes <abhinandtk.primalcodes@gmail.com>
2026-02-21 18:00:32 +0530

    database added

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product,ProductVariant,Color,Size
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Color
+        fields="__all__"
+class SizeSerailizer(serializers.ModelSerializer):
+    class Meta:
+        model=Size
+        fields="__all__"
+class ProductVariantSerializer(serializers.ModelSerializer):
+    sizes=SizeSerailizer(read_only=True)
+    colors=ColorSerializer(read_only=True)
+    class Meta:
+        model=ProductVariant
+        fields="__all__"
 
 class ProductSerializer(serializers.ModelSerializer):
+    variants=ProductVariantSerializer(many=True,read_only=True)
     class Meta:
         model=Product
         fields="__all__"
